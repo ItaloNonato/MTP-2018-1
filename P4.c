b3d6b744347b0984687e2c7fd0f8f47a3b9c36c3
Italo Dias <37388669+ItaloNonato@users.noreply.github.com>
2018-05-06 15:57:38 -0300

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//Italo Dias Nonato
+//11711EEL031
+int main(){
+	int i=0,cum=0;
+	char n[256];
+	printf("Digite a sequencia de caracteres: ");
+	scanf("%s",&n); getchar();
+	while(n[i]!='\0'){
+		if(n[i]=='0'||n[i]=='1'||n[i]=='2'||n[i]=='3'||n[i]=='4'||n[i]=='5'||n[i]=='6'||n[i]=='7'||n[i]=='8'||n[i]=='9')
+		cum=cum*10+n[i]-'0';
+		i++;
+	}
+	printf("%d",cum);
+	return 0;
+}
