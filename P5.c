61b40fd3078d7c71567c1a6bb83344f1527c2cac
Italo Dias <37388669+ItaloNonato@users.noreply.github.com>
2018-05-14 21:24:14 -0300

    Add files via upload

---
+++ P5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+//Italo Dias Nonato
+//11711EEl031
+int main(){
+	char s[256];
+	int *pi=(int *)s,i,n,a[64],b,aux;
+	printf("   Menu\n1.Codificar\n2.Decodificar\nEscolha uma opcao: ");
+	scanf("%d",&n);
+	getchar();
+if(n==1){
+	printf("Digite uma mensagem: ");
+	for(i=0;i<255;i++){
+		s[i]=getchar();
+		if(s[i]==10)
+			break;
+	}
+	s[i]='\0';
+	if(i%4==0){
+		for(b=0;b<(i/4)-1;b++)
+			printf("%d, ",pi[b]);
+		printf("%d",pi[b]);
+		}
+	else{
+		for(b=0;b<(i/4);b++)
+			printf("%d, ",pi[b]);
+		printf("%d",pi[b]);
+			}
+	}
+if(n==2){
+		printf("Digite um codigo numerico: ");
+		for(i=0; i<64; i++){
+			scanf("%d%c",&a[i],&aux);
+			if(aux!=',')
+				break;
+		}
+		printf("%s",&a);
+	}	
+	return 0;
+}
