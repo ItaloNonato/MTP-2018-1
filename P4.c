97c52572b9bc4e70f7e6eb598c69acf0a00a612b
Italo Dias <37388669+ItaloNonato@users.noreply.github.com>
2018-06-21 00:46:57 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -5,7 +5,7 @@ int main(){
 	int i=0,cum=0;
 	char n[256];
 	printf("Digite a sequencia de caracteres: ");
-	scanf("%s",&n); getchar();
+	scanf("%s",n); getchar();
 	while(n[i]!='\0'){
 		if(n[i]=='0'||n[i]=='1'||n[i]=='2'||n[i]=='3'||n[i]=='4'||n[i]=='5'||n[i]=='6'||n[i]=='7'||n[i]=='8'||n[i]=='9')
 		cum=cum*10+n[i]-'0';
