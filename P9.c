36b42bd4f75fee33595acdbd843b7bfac1bb424f
Italo Dias <37388669+ItaloNonato@users.noreply.github.com>
2018-06-30 17:05:20 -0300

    Add files via upload

---
+++ P9.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//Italo Dias Nonato
+//11711EEL031
+struct Dimensoes{
+	float largura;
+	float profundidade;
+	float altura;
+};
+	typedef
+struct meuProduto{
+	char nome[64];
+	float preco;
+	struct Dimensoes d;	
+}Produto;
+Produto dados[2];
+void cadastrar(int i){
+			printf("Nome= ");
+			fgets(dados[i].nome,64,stdin);
+			printf("Preco= ");
+			scanf("%f",&dados[i].preco);
+			printf("Largura= ");
+			scanf("%f",&dados[i].d.largura);
+			printf("Profundidade= ");
+			scanf("%f",&dados[i].d.profundidade);
+			printf("Altura= ");
+			scanf("%f",&dados[i].d.altura);
+			printf("\nProduto %d cadastrado com sucesso\n\n",i);
+}
+void consultar(int i){
+	if(strlen(dados[i].nome) == 0){
+		printf("Produto nao cadastrado!\n\n");
+}
+	else
+	printf("%s, R$ %.2f, L: %.1fm x P: %.2fm x A: %.2fm\n",dados[i].nome,dados[i].preco,dados[i].d.largura,dados[i].d.profundidade,dados[i].d.altura);
+}
+int main() {
+	int n,m,i,j=0;
+	Produto P;
+	for(i=0;i<2;i++){
+		strcpy(dados[i].nome, "");
+	}
+while(n!=3){
+	printf("    MENU\n1.Cadastro\n2.Consulta\n3.Sair\nDigite uma opcao: ");
+	scanf("%d",&n);getchar();
+		if(n==1){
+		printf("Produto 1 ou 2: ");
+		scanf("%d",&m);getchar();
+		cadastrar(m);
+	}
+		else if(n==2){
+			printf("Produto 1 ou 2: ");
+			scanf("%d",&m);getchar();
+			consultar(m);
+	}
+}
+	return 0;
+}
