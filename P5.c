25b3ee1f864e1a26e54d035341fe7fd35b8334f7
Italo Dias <37388669+ItaloNonato@users.noreply.github.com>
2018-07-10 00:16:25 -0300

    Update P5.c

--- P5.c
+++ P5.c
@@ -1,10 +1,10 @@
-//Nome: Italo Dias Nonato
-//Matricula: 11711EEl031
+//Italo Dias Nonato
+//11711EEL031
 #include <stdio.h>
 int main(){
-	int a[64],i,n,b;;
-	char s[256],*ps=(char*)a,c;
-	int *pi=(int *)s;
+	char s[256],c;
+	int *pi=(int *)s,i,n,b,a[64];
+	char *ps=(char *)a;
 	printf("   Menu\n1.Codificar\n2.Decodificar\nEscolha uma opcao: ");
 	scanf("%d",&n);
 	getchar();
@@ -16,25 +16,17 @@ if(n==1){
 			break;
 	}
 	s[i]='\0';
-	if(i%4==0){
-		for(b=0;b<(i/4)-1;b++)
-			printf("%d, ",pi[b]);
-		printf("%d",pi[b]);
-		}
-	else{
-		for(b=0;b<(i/4);b++)
-			printf("%d, ",pi[b]);
-		printf("%d",pi[b]);
-			}
+		for(b=0;b<=(i/4);b++)
+			printf("%d ",pi[b]);
 	}
 if(n==2){
 		printf("Digite um codigo numerico: ");
-		for(i=0; i<64; i++){
+		for(i=0;i<64;i++){
 			scanf("%d%c",&a[i],&c);
-			if(c!=',')
-				break;
+			if(c==10)
+			break;
 		}
 		printf("%s",ps);
-	}	
+	}
 	return 0;
 }
